eeb412b255744bfa37c33cb0501f915c9ca5dd5c
Kanishka <kanishka.it27@gmail.com>
2026-07-17 18:10:10 +0530

    Time: 3 ms (87.57%), Space: 47.5 MB (69.24%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] dp = new int[ratings.length];
+        Arrays.fill(dp , 1);
+
+        for(int i = 1 ;i<ratings.length;i++){
+            if(ratings[i] > ratings[i-1]){
+                dp[i] = 1 + dp[i-1];
+            }
+        }
+        for(int i = ratings.length - 2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                dp[i] = Math.max(dp[i] , dp[i+1]+1);
+            }
+        }
+        int tot = 0;
+        for(int i : dp){
+            tot += i;
+        }
+        return tot;
+    }
+}
